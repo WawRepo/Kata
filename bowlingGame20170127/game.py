cc790d59d1855d265a2cacb8b1ef1e75ddeeeb39
WawRepo <wawa_666@yahoo.com>
2017-01-29 15:13:58 +0100

    roll, score, splilt + initial testing

--- bowlingGame20170127/game.py
+++ bowlingGame20170127/game.py
@@ -1,3 +1,32 @@
-Class Game:
-    def fun(self):
-        pass
+class Game:
+    def __init__(self):
+        self.rolls = []
+
+    def Roll(self, pins):
+        if pins >= 0 and pins <= 10:
+            self.rolls.append(pins)
+            return pins
+
+    def SplitIntoFrames(self):
+        frames = []
+        frame = []
+        for roll in self.rolls:
+            if roll == 10:
+                frame.append(roll)
+                frames.append(frame)
+                frame = []
+            else:
+                frame.append(roll)
+                if len(frame) == 2:
+                    frames.append(frame)
+                    frame =[]
+
+        if len(frame) > 0:
+            frames.append(frame)
+        return frames
+
+    def Score(self):
+        score = 0
+        for roll in self.rolls:
+            score += roll
+        return score
--- bowlingGame20170127/test_game.py
+++ bowlingGame20170127/test_game.py
@@ -1,9 +1,58 @@
-#from __future__ import absolute_import
 from unittest import TestCase
 
 from game import Game
 
 class TestGame(TestCase):
-    def testCreateGame(self):
+
+    def testRoll(self):
         game = Game()
-        self.assertEquals(1,2)
+        game.Roll(1)
+
+    def testRollSize(self):
+        game = Game()
+        self.assertEquals(game.Roll(1) , 1)
+
+        self.assertIsNone(game.Roll(-1))
+        self.assertIsNone(game.Roll(11))
+
+    def testScore(self):
+        game = Game()
+        self.assertEquals(0,game.Score())
+        game.Roll(1)
+        self.assertEquals(1,game.Score())
+
+    def roolMany(self,game,pins,n):
+        for i in range(0,n,1):
+            game.Roll(pins)
+
+    def testRollMultipleSamePinsNo(self ):
+        sameRoll = [0,1,2,3,4]
+        for pins in sameRoll:
+            game = Game()
+            self.roolMany(game,pins,20)
+            self.assertEquals(20*pins, game.Score())
+
+    def testSplitIntoFrames(self):
+        game = Game()
+        game.Roll(1)
+        self.assertListEqual([[1]],game.SplitIntoFrames())
+        game.Roll(3)
+        self.assertListEqual([[1,3]], game.SplitIntoFrames())
+        game.Roll(10)
+        self.assertListEqual([[1, 3],[10]], game.SplitIntoFrames())
+        game.Roll(1)
+        self.assertListEqual([[1, 3], [10],[1]], game.SplitIntoFrames())
+
+
+    # def testStrike(self):
+    #     game = Game()
+    #     game.Roll(5)
+    #     game.Roll(5)
+    #     game.Roll(3)
+    #     self.assertEquals(16, game.Score())
+    #
+    # def testImproperThrows(self):
+    #     game = Game()
+    #     game.Roll(5)
+    #     self.assertIsNone(game.Roll(6))
+
