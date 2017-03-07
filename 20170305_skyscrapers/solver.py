731ce8220dd4febd0104c29c98dae8d7815ecd56
WawRepo <wawa_666@yahoo.com>
2017-03-07 13:15:18 +0100

    skyscraper
    refactor

--- 20170305_skyscrapers/solver.py
+++ 20170305_skyscrapers/solver.py
@@ -1,34 +1,22 @@
-#https://www.codewars.com/kata/4-by-4-skyscrapers
-
 import copy
 
-def solve_scrapers(hints, board=[[]]):
+
+def solve_puzzle(hints, board=[[]]):
+    """    https://www.codewars.com/kata/4-by-4-skyscrapers    """
 
     if len(board) == 1:
-        board = [[ 0 for j in range(0, 4)] for i in range(0, 4)]
-    available_values_len_tab = [[[available_values(i, j, board), len(available_values(i, j, board))]
-                                 for j in range(0, 4)] for i in range(0, 4)]
+        board = [[0 for j in range(0, 4)] for i in range(0, 4)]
 
-    min_len = 5
-    x, y = None, None
-    for i in range(0, 4):
-        for j in range(0, 4):
-            if available_values_len_tab[i][j][1] > 0 and board[i][j] == 0:
-                if min_len > available_values_len_tab[i][j][1]:
-                    min_len = available_values_len_tab[i][j][1]
-                    x, y = i, j
-            #game unsolvable
-            elif available_values_len_tab[i][j][1] == 0 and board[i][j] == 0:
-                return False
+    available_values_len_tab = get_avaialble_values(board)
+    x, y = get_next_position_with_minimal_len(board, available_values_len_tab)
 
     # validate vs hints:
     for i, hint_value in enumerate(hints):
         if not validate_view_by_hint(view_for_hint(i, board), hint_value):
             return False
 
-    #solution
+    # solution
     if x is None and y is None:
-        # print  len(solutions), solutions
         return board
 
     next_board = copy.deepcopy(board)
@@ -38,8 +26,27 @@ def solve_scrapers(hints, board=[[]]):
             return False
         else:
             next_board[x][y] = available_values_len_tab[x][y][0].pop()
-            solution = solve_scrapers(hints, next_board)
+            solution = solve_puzzle(hints, next_board)
     return tuple([tuple(x) for x in solution])
+
+
+def get_avaialble_values(board):
+    available_values_len_tab = [[[available_values(i, j, board), len(available_values(i, j, board))]
+                                 for j in range(0, 4)] for i in range(0, 4)]
+    return available_values_len_tab
+
+
+def get_next_position_with_minimal_len(board, available_values_len_tab):
+    min_len = 5
+    x, y = None, None
+    for i in range(0, 4):
+        for j in range(0, 4):
+            if available_values_len_tab[i][j][1] > 0 and board[i][j] == 0:
+                if min_len > available_values_len_tab[i][j][1]:
+                    min_len = available_values_len_tab[i][j][1]
+                    x, y = i, j
+    return x, y
+
 
 def visible_count(view):
     top = view[0]
@@ -50,25 +57,30 @@ def visible_count(view):
             top = building
     return view_ct
 
-def get_horizontal_values(i, j, problem):
+
+def get_horizontal_values(i, problem):
     return {x for x in problem[i] if x != 0}
 
-def get_vertical_values(i, j, problem):
+
+def get_vertical_values(j, problem):
     return {problem[x][j] for x in range(0, 4) if problem[x][j] != 0}
+
 
 def available_values(i, j, problem):
     return {j for j in range(1, 5)} - (
-        get_horizontal_values(i, j, problem) | get_vertical_values(i, j, problem))
+        get_horizontal_values(i, problem) | get_vertical_values(j, problem))
+
 
 def view_for_hint(i, problem):
-    if i >= 0 and i <= 3:
+    if 0 <= i <= 3:
         return [problem[x][i] for x in range(0, 4)]
-    if i >= 4 and i <= 7:
+    if 4 <= i <= 7:
         return [x for x in problem[i % 4]][::-1]
-    if i >= 8 and i <= 11:
+    if 8 <= i <= 11:
         return [problem[x][3 - i % 8] for x in range(0, 4)][::-1]
-    if i >=12 and i <= 15:
+    if 12 <= i <= 15:
         return [x for x in problem[3 - i % 12]]
+
 
 def validate_view_by_hint(view, hint_value):
     if view.count(0) == 0 and hint_value != 0:
--- 20170305_skyscrapers/test_solver.py
+++ 20170305_skyscrapers/test_solver.py
@@ -33,9 +33,18 @@ class TestSolve(TestCase):
             (0, 1, 3, 4)
         )
 
+        self.board = [[1, 2, 4, 3], [2, 1, 3, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+        self.available_values_len_tab = [[[set([]), 0], [set([]), 0], [set([]), 0], [set([]), 0]]
+            , [[set([4]), 1], [set([4]), 1], [set([]), 0], [set([4]), 1]]
+            , [[set([3, 4]), 2], [set([3, 4]), 2], [set([1, 2]), 2], [set([1, 2, 4]), 3]]
+            , [[set([3, 4]), 2], [set([3, 4]), 2], [set([1, 2]), 2], [set([1, 2, 4]), 3]]]
+
+        self.unsolvable_board = [[2, 1, 4, 3], [3, 4, 1, 2], [4, 2, 3, 1], [1, 3, 0, 4]]
+
+
     def test_solve_scrapers(self):
-        self.assertEqual(solve_scrapers(self.clues[0]), self.outcomes[0])
-        self.assertEqual(solve_scrapers(self.clues[1]), self.outcomes[1])
+        self.assertEqual(solve_puzzle(self.clues[0]), self.outcomes[0])
+        self.assertEqual(solve_puzzle(self.clues[1]), self.outcomes[1])
 
 
     def test_visible_count(self):
@@ -56,17 +65,28 @@ class TestSolve(TestCase):
         self.assertTrue(validate_view_by_hint([1, 3, 4, 2], 3))
 
     def test_get_horizontal_values(self):
-        self.assertEqual(get_horizontal_values(0, 0, self.validation), {1, 2})
-        self.assertEqual(get_horizontal_values(1, 1, self.validation), {3})
-        self.assertEqual(get_horizontal_values(3, 3, self.validation), {1, 3, 4})
+        self.assertEqual(get_horizontal_values(0, self.validation), {1, 2})
+        self.assertEqual(get_horizontal_values(1, self.validation), {3})
+        self.assertEqual(get_horizontal_values(3, self.validation), {1, 3, 4})
 
     def test_get_vertical_values(self):
-        self.assertEqual(get_vertical_values(0, 0, self.validation), {1})
-        self.assertEqual(get_vertical_values(1, 1, self.validation), {1})
-        self.assertEqual(get_vertical_values(3, 3, self.validation), {2, 3, 4})
+        self.assertEqual(get_vertical_values(0, self.validation), {1})
+        self.assertEqual(get_vertical_values(1, self.validation), {1})
+        self.assertEqual(get_vertical_values(3, self.validation), {2, 3, 4})
 
     def test_available_values(self):
         self.assertEqual(available_values(0, 0, self.validation), {3, 4})
         self.assertEqual(available_values(1, 1, self.validation), {2, 4})
         self.assertFalse(len(available_values(3, 3, self.validation)))
+
+    def test_get_avaialble_values(self):
+        self.assertEqual(get_avaialble_values(self.board), self.available_values_len_tab)
+
+    def test_get_next_position_with_minimal_len(self):
+        self.assertEqual(get_next_position_with_minimal_len(self.board, self.available_values_len_tab), (1,3))
+
+        #for solved game
+        self.assertEqual(get_next_position_with_minimal_len(self.outcomes[0]
+                                                            , get_avaialble_values(self.outcomes[0]))
+                         , (None, None))
 
