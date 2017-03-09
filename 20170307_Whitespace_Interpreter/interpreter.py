cd5060992d65161f0e834a1e97fedfbc2d05958b
WawRepo <wawa_666@yahoo.com>
2017-03-09 23:45:09 +0100

    Whitespace_Interpreted
    intial stack manipulation

--- 20170307_Whitespace_Interpreter/interpreter.py
+++ 20170307_Whitespace_Interpreter/interpreter.py
@@ -1,3 +1,78 @@
+# IMP [space] - Stack Manipulation
+#
+# [space] (number): Push n onto the stack.
+# [tab][space] (number): Duplicate the nth value from the top of the stack.
+# [tab][line-feed] (number): Discard the top n values below the top of the stack from the stack. (For n<0 or n>=stack.length, remove everything but the top value.)
+# [line-feed][space]: Duplicate the top value on the stack.
+# [line-feed][tab]: Swap the top two value on the stack.
+# [line-feed][line-feed]: Discard the top value on the stack.
+
+
+def code_reader(code):
+    """Code char generator"""
+    for c in code:
+        yield c
+
+def read_until_terminal(code_reader):
+    output_str = ''
+    for c in code_reader:
+        if c == 'n':
+            break
+        output_str += c
+    return output_str
+
+
+def stack_manipulation(code_reader, stack):
+    control_value = code_reader.next()
+    if not stack_dict.has_key(control_value):
+        control_value += code_reader.next()
+    (stack_dict[control_value])(code_reader, stack)
+
+
+def stack_manipulation_push(code_reader, stack):
+    to_push = read_until_terminal(code_reader)
+    stack.append(parse_number(to_push))
+
+def stack_manipulation_duplicate_nth(code_reader, stack):
+    item_to_duplicate = len(stack) - parse_number(read_until_terminal(code_reader))
+    stack[:] = stack[:item_to_duplicate] + [stack[item_to_duplicate]] + stack[item_to_duplicate:]
+
+# IMP [space] - Stack Manipulation
+#
+# [space] (number): Push n onto the stack.
+# [tab][space] (number): Duplicate the nth value from the top of the stack.
+# [tab][line-feed] (number): Discard the top n values below the top of the stack from the stack. (For n<0 or n>=stack.length, remove everything but the top value.)
+# [line-feed][space]: Duplicate the top value on the stack.
+# [line-feed][tab]: Swap the top two value on the stack.
+# [line-feed][line-feed]: Discard the top value on the stack.
+
+
+stack_dict = {
+    's': stack_manipulation_push
+    , 'ts': stack_manipulation_duplicate_nth
+    , 'tt': "Heap Access"
+    , 'tn': "Input/Output"
+    , 'n': "Flow Control"
+}
+
+def parse_number(number_wi_syntax):
+    if number_wi_syntax == '':
+        raise ValueError('Empty string not allowed')
+    factor = 1 if number_wi_syntax[0] == 's' else -1
+    return factor * int("".join([ '1' if c == 't' else '0' for c in number_wi_syntax[1:]]), 2)
+
+
+
+
+imp_dict = {
+    ' ': "Stack Manipulation"
+    , '\t ': "Arithmetic"
+    , '\t\t': "Heap Access"
+    , '\t\n': "Input/Output"
+    , '\n': "Flow Control"
+}
+
+
 
 def whitespace(code, inp=''):
     """  https://www.codewars.com/kata/whitespace-interpreter/train/python """
--- 20170307_Whitespace_Interpreter/test_interpreter.py
+++ 20170307_Whitespace_Interpreter/test_interpreter.py
@@ -3,37 +3,88 @@ from interpreter import *
 
 class TestSolve(TestCase):
 
-
-    def test_output_0_3(self):
-        """ Testing push, output of numbers 0 through 3 """
-        output1 = "   \t\n\t\n \t\n\n\n"
-        output2 = "   \t \n\t\n \t\n\n\n"
-        output3 = "   \t\t\n\t\n \t\n\n\n"
-        output0 = "    \n\t\n \t\n\n\n"
-        self.assertEquals(whitespace(output1), "1")
-        self.assertEquals(whitespace(output2), "2")
-        self.assertEquals(whitespace(output3), "3")
-        self.assertEquals(whitespace(output0), "0")
+    def test_code_reader(self):
+        code = '123'
+        reader = code_reader(code)
+        self.assertEquals(reader.next(), '1')
+        self.assertEquals(reader.next(), '2')
+        self.assertEquals(reader.next(), '3')
+        self.assertRaises(StopIteration, lambda: reader.next())
 
 
-    def test_output_negative_1_3(self):
-        """ Testing ouput of numbers -1 through -3 """
-        outputNegative1 = "  \t\t\n\t\n \t\n\n\n"
-        outputNegative2 = "  \t\t \n\t\n \t\n\n\n"
-        outputNegative3 = "  \t\t\t\n\t\n \t\n\n\n"
-        self.assertEquals(whitespace(outputNegative1), "-1")
-        self.assertEquals(whitespace(outputNegative2), "-2")
-        self.assertEquals(whitespace(outputNegative3), "-3")
+    def test_read_until_terminal(self):
+        code = '123nzcxn'
+        reader = code_reader(code)
+        self.assertEquals(read_until_terminal(reader), '123')
+        self.assertEquals(read_until_terminal(reader), 'zcx')
+
+    def test_parse_number(self):
+        """Testing parsing mechanic"""
+        number_wi = 'st'
+        self.assertEquals(parse_number(number_wi), 1)
+        number_wi = 'tt'
+        self.assertEquals(parse_number(number_wi), -1)
+        number_wi = 'ttss'
+        self.assertEquals(parse_number(number_wi), -4)
+        number_wi = ''
+        self.assertRaises(Exception,lambda: parse_number(number_wi))
+
+
+    def test_stack_manipulation_push(self):
+        code = 'st'
+        stack = []
+        expected_stack = [1]
+        stack_manipulation_push(code, stack)
+        self.assertEquals(stack, expected_stack)
+
+    def test_stack_manipulation_duplicate_nth(self):
+        stack = [1, 2, 3, 4]
+        code = 'stsn'#2
+        code_rdr = code_reader(code)
+        stack_manipulation_duplicate_nth(code_rdr, stack)
+        self.assertEqual(stack, [1, 2, 3, 3, 4])
+
+    def test_stack_manipulation_discard_n(self):
+
+    def test_stack_manipulation(self):
+        code = 'sstnss'
+        code_rdr = code_reader(code)
+        stack = []
+        expected_stack = [1]
+        stack_manipulation(code_rdr, stack)
+        self.assertEquals(stack, expected_stack)
+
+
+    # def test_output_0_3(self):
+    #     """ Testing push, output of numbers 0 through 3 """
+    #     output1 = "   \t\n\t\n \t\n\n\n"
+    #     output2 = "   \t \n\t\n \t\n\n\n"
+    #     output3 = "   \t\t\n\t\n \t\n\n\n"
+    #     output0 = "    \n\t\n \t\n\n\n"
+    #     self.assertEquals(whitespace(output1), "1")
+    #     self.assertEquals(whitespace(output2), "2")
+    #     self.assertEquals(whitespace(output3), "3")
+    #     self.assertEquals(whitespace(output0), "0")
+    #
+    #
+    # def test_output_negative_1_3(self):
+    #     """ Testing ouput of numbers -1 through -3 """
+    #     outputNegative1 = "  \t\t\n\t\n \t\n\n\n"
+    #     outputNegative2 = "  \t\t \n\t\n \t\n\n\n"
+    #     outputNegative3 = "  \t\t\t\n\t\n \t\n\n\n"
+    #     self.assertEquals(whitespace(outputNegative1), "-1")
+    #     self.assertEquals(whitespace(outputNegative2), "-2")
+    #     self.assertEquals(whitespace(outputNegative3), "-3")
 
     def test_error_for_empty_code(self):
         """Testing simple flow control edge case"""
         self.assertRaises(Exception, lambda: whitespace(''))
 
-    def test_letter_output_A_C(self):
-        """Testing output of letters A through C"""
-        outputA = "   \t     \t\n\t\n  \n\n\n"
-        outputB = "   \t    \t \n\t\n  \n\n\n"
-        outputC = "   \t    \t\t\n\t\n  \n\n\n"
-        self.assertEquals(whitespace(outputA), "A")
-        self.assertEquals(whitespace(outputB), "B")
-        self.assertEquals(whitespace(outputC), "C")
+    # def test_letter_output_A_C(self):
+    #     """Testing output of letters A through C"""
+    #     outputA = "   \t     \t\n\t\n  \n\n\n"
+    #     outputB = "   \t    \t \n\t\n  \n\n\n"
+    #     outputC = "   \t    \t\t\n\t\n  \n\n\n"
+    #     self.assertEquals(whitespace(outputA), "A")
+    #     self.assertEquals(whitespace(outputB), "B")
+    #     self.assertEquals(whitespace(outputC), "C")
